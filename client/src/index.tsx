9f03fd7865d46e31bfdf6ecfdb7a5f81d02d63e3
Tiger Hong <tigerhong@Tigers-MBP.lan>
2024-03-12 17:06:00 -0700

    commit:
    basic file structure created and React App in .tsx is running fine.

---
+++ client/src/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+// import App from './components/app.ts';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<h1> test </h1>);
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: path.resolve(__dirname, "client", "src", "index.tsx"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  devServer: {
+    static: [
+    {
+      directory: path.resolve(__dirname, "client", "src", "dist"),
+    }
+    ],
+    hot: true,
+    port: 3000,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      // babel loaders here
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+            plugins: ["module:react-native-dotenv"]
+          }
+        }
+      },
+      // css loaders here
+      {
+        test: /\.(css)$/,
+        use: [
+          {
+            // Adds CSS to the DOM by injecting a `<style>` tag
+            loader: 'style-loader'
+          },
+          {
+            // Interprets `@import` and `url()` like `import/require()` and will resolve them
+            loader: 'css-loader'
+          },
+        ]
+      },
+      {
+        test: /\.(pdf|png|jpe?g|gif|svg|md|html)$/i,
+        type: 'asset/resource',
+      },
+    ]
+  },
+}
